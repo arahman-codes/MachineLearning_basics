0085ab0a12f106a0481c444b73400d72b6daa80c
Ajijur Rahman <arahman.codes@gmail.com>
2026-06-14 10:22:20 +0530

    Today revise Lists in python

---
+++ List/Basics_List.py
@@ -0,0 +1,16 @@
+list1 = ['hi','Mi Amor' ,'Love']
+print(list)
+print(list[0])
+print(list[1])
+print(list[2])
+#Mixed list....
+list2 = ['Mi Ammor' , '27sept' , '7th feb' , '28th feb mistake']
+print(list2)
+list2[0] = 'Saumya Pandey'
+print(list2)
+list2.append('just love can fix me...')
+print(list2)
+list2.insert(1,"Te Amo")
+print(list2)
+list2.remove("Saumya Pandey")
+print(list2)
---
+++ Strings/Strings_basics.py
@@ -0,0 +1,17 @@
+Love = "Mi Amour"
+print(Love)
+# #Indexing in python....
+print(Love[0],Love[4])
+print(Love[1])
+print(Love[3])
+print(Love[2])
+#Positive slicin....
+print(Love[0:7])
+print(Love[:7])
+print(Love[:])
+print(Love[0:8])
+#Negative Slicing....
+#in negative silicing you have to use just negative number even you can't use 0 ...
+print(Love[-8:-1:1])
+print(Love[-8:-1:-1])
+print(Love[:-1:-1])
